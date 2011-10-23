dc798f08aea70482b4bc082ced45c8a4edb9eda8
Alex Kalderimis <alex@flymine.org>
2011-10-23 23:04:30 +0100

    Trimmed down what is supplied here to the minimum

--- lib/Bio/GMOD/RPC/Provider/InterMine/Organisms/v1_1.pm
+++ lib/Bio/GMOD/RPC/Provider/InterMine/Organisms/v1_1.pm
@@ -1,12 +1,22 @@
 package Bio::GMOD::RPC::Provider::InterMine::Organisms::v1_1;
 
-use Moose;
+use strict;
+use warnings;
 
-with 'Bio::GMOD::RPC::InterMine::Role::Configured';
+use Moose;
+use Webservice::InterMine;
+
+with 'Bio::GMOD::RPC::Provider';
 
 sub get_data {
     my $self = shift;
-    my $params = shift;
+    my %params = @_;
+
+    my $im = get_service($self->service->config->{intermine}{server});
+
+    my $query = $im->new_query(class => "Organism")->select(qw/genus species taxonId/);
+
+    [map { {genus => $_->[0], species => $_->[1], taxonomy_id => $_->[2] } } $query->all];
 }
 
 1;
